b25310734454e68f462b7d2f66798b887bd5ebeb
calebe werneck couto <calebewerneck@hotmail.com>
2023-09-26 18:08:13 -0300

    atualizar

--- springboot/src/main/java/curso/springboot/controller/UsuarioController.java
+++ springboot/src/main/java/curso/springboot/controller/UsuarioController.java
@@ -7,10 +7,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import curso.springboot.model.Pessoa;
+import curso.springboot.model.Profissao;
+import curso.springboot.model.Usuario;
 import curso.springboot.repository.UsuarioRepository;
 
 @Controller
@@ -20,18 +25,76 @@ public class UsuarioController {
 	private UsuarioRepository usuarioRepository;
 	
 	
-	@GetMapping("/addusuario/")
-	public ModelAndView documentos(@RequestParam("login") String login, @RequestParam("senha")String senha,@RequestParam("nome")String nome) {
+	
+	
+	
+	
+	@PostMapping("**/addusuario/")
+	public ModelAndView addFonePessoa(@RequestParam("login") String login,@RequestParam("senha") String senha,@RequestParam("nome") String nome) {
 		
 		
+		Usuario usuario = new Usuario();
+				ModelAndView modelAndView = new ModelAndView("cadastro/usuarios");
 
-		ModelAndView modelAndView = new ModelAndView("cadastro/cadastrousuarios");
 		
+		usuario.setLogin(login);
+		usuario.setSenha(senha);
+		usuario.setNome(nome);
+		
+		
+		usuarioRepository.save(usuario);
+		
+		
+		
+		
+		
+		
+		modelAndView.addObject("usuarios", usuarioRepository.findAll());
+
+		return modelAndView;
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	@RequestMapping(method = RequestMethod.GET, value = "/usuarios")
+	public ModelAndView pessoas() {
+	
+
+
+		ModelAndView modelAndView = new ModelAndView("cadastro/usuarios");
+		modelAndView.addObject("pessoaobj", new Pessoa());
 		modelAndView.addObject("msg", new ArrayList<String>());
 		modelAndView.addObject("usuarios", usuarioRepository.findAll());
 		return modelAndView;
 		
 	}
+	
+	@GetMapping("/removerusuario/{idusuario}")
+	public ModelAndView removerusuario(@PathVariable("idusuario") Long idusuario) {
+		
+		
+		
+		usuarioRepository.deleteById(idusuario);
+		
+		ModelAndView modelAndView = new ModelAndView("cadastro/usuarios");
+		modelAndView.addObject("pessoaobj", new Pessoa());
+	
+		modelAndView.addObject("usuarios", usuarioRepository.findAll());
+		return modelAndView;
+		
+	}
+	
+		
+	}
 
 
-}
+
